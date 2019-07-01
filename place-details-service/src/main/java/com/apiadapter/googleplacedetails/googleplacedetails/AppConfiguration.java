d485c50042ed980797a69a95c8ce2acff6d1a4fb
sinch5 <sinch5@yandex.ru>
2019-07-01 19:14:19 +0400

    added services

---
+++ place-details-service/src/main/java/com/apiadapter/googleplacedetails/googleplacedetails/AppConfiguration.java
@@ -0,0 +1,19 @@
+package com.apiadapter.googleplacedetails.googleplacedetails;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class AppConfiguration {
+
+    @Value("${base.googlemaps.url}")
+    private String routes;
+
+    @Bean
+    public WebClient getWebClient() {
+        return  WebClient.create(routes);
+    }
+
+}
