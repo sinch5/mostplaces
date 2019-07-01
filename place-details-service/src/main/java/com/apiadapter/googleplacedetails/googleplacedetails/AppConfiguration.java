d73eb3366cbea121959eee2bb541bbd9d78bcb14
sinch5 <sinch5@yandex.ru>
2019-07-01 19:51:48 +0400

    add services

--- place-details-service/src/main/java/com/apiadapter/googleplacedetails/googleplacedetails/AppConfiguration.java
+++ place-details-service/src/main/java/com/apiadapter/googleplacedetails/googleplacedetails/AppConfiguration.java
@@ -11,6 +11,12 @@ public class AppConfiguration {
     @Value("${base.googlemaps.url}")
     private String routes;
 
+    @Value("${placeDetails.url}")
+    private String placeDetailsUrl;
+
+    @Value("${googlemaps.api.key}")
+    private String googlemapsApiKey;
+
     @Bean
     public WebClient getWebClient() {
         return  WebClient.create(routes);
