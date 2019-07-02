6bede4e00925ecbb4e421841b4cf49c12eda27b8
sinch5 <sinch5@yandex.ru>
2019-07-02 17:46:10 +0400

    fix

--- place-details-service/src/main/java/com/apiadapter/googleplacedetails/googleplacedetails/StartupApplicationListenerExample.java
+++ place-details-service/src/main/java/com/apiadapter/googleplacedetails/googleplacedetails/StartupApplicationListenerExample.java
@@ -7,6 +7,9 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+/**
+ * 
+ */
 @Component
 public class StartupApplicationListenerExample implements ApplicationListener<ContextRefreshedEvent> {
 
