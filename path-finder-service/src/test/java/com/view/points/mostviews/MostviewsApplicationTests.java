2273b55e13f88a061c5966cefb5d2326512a838a
sinch5 <sinch5@yandex.ru>
2019-07-02 16:31:40 +0400

    Update MostviewsApplicationTests.java

--- path-finder-service/src/test/java/com/view/points/mostviews/MostviewsApplicationTests.java
+++ path-finder-service/src/test/java/com/view/points/mostviews/MostviewsApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = MostviewsApplicationTests.class)
+@SpringBootTest(classes = MostviewsApplicationTests.class, properties = "spring.cloud.config.enabled=false")
 public class MostviewsApplicationTests {
 
 	@Test
